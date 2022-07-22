877db103215c503a4660a54d4bcc4623e361e82e
manoss96 <manosstoumpos@gmail.com>
2022-07-22 23:03:38 +0300

    updates

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../src/pregex/'))
+sys.path.insert(1, os.path.abspath('../src/pregex/'))
+sys.path.insert(2, os.path.abspath('src/pregex/'))
 
 
 # -- Project information -----------------------------------------------------
